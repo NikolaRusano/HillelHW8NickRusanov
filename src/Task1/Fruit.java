00992ecc284eebe9522ea4c29478e7cf20b83692
Nikolay Rusanov <nikolarusano92@gmail.com>
2021-05-23 23:04:34 +0300

    Initial commit

---
+++ src/Task1/Fruit.java
@@ -0,0 +1,17 @@
+package Task1;
+
+public class Fruit {
+    int calories;
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void makeJuice(){
+        System.out.println("The juice have been done");
+    }
+}
